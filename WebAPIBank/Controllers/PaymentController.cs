ab77458f3be25438996d9706b805b96afb67a0d8
naciyeceren097 <naciye.ceren097@gmail.com>
2021-10-12 10:48:20 +0300

    Add project files.

---
+++ WebAPIBank/Controllers/PaymentController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIBank.DesignPatterns.SingletonPattern;
+using WebAPIBank.DTOClasses;
+using WebAPIBank.Models.Context;
+using WebAPIBank.Models.Entities;
+
+namespace WebAPIBank.Controllers
+{
+    public class PaymentController : ApiController
+    {
+        MyContext _db;
+        public PaymentController()
+        {
+            _db = DBTool.DBInstance;
+        }
+        //[HttpGet]
+        //public List<PaymentDTO> GetAll()
+        //{
+        //    return _db.Cards.Select(x => new PaymentDTO
+        //    {
+        //        CardUserName = x.CardUserName
+        //    }).ToList();
+        //}
+
+        [HttpPost]
+        public IHttpActionResult ReceivePayment(PaymentDTO item)
+        {
+            CardInfo ci = _db.Cards.FirstOrDefault(x => x.CardNumber == item.CardNumber && x.SecurityNumber == item.SecurityNumber && x.CardUserName == item.CardUserName && x.CardExpiryYear == item.CardExpiryYear && x.CardExpiryMonth == item.CardExpiryMonth);
+
+            if(ci != null)
+            {
+                if (ci.CardExpiryYear < DateTime.Now.Year)
+                {
+                    return BadRequest("Expired Card");
+                }
+                else if(ci.CardExpiryYear == DateTime.Now.Year)
+                {
+                    if(ci.CardExpiryMonth < DateTime.Now.Month)
+                    {
+                        return BadRequest("Expired card");
+                    }
+
+                    if(ci.Balance >= item.ShoppingPrice)
+                    {
+                        SetBalance(item, ci);
+                        return Ok();
+                    }
+                    else
+                    {
+                        return BadRequest("Balance Exceeded");
+                    }
+                }
+
+                if (ci.Balance >= item.ShoppingPrice)
+                {
+                    SetBalance(item, ci);
+                    return Ok();
+                }
+                return BadRequest("Balance exceeded");
+            }
+
+            return BadRequest("Card not found");
+        }
+        private void SetBalance(PaymentDTO item, CardInfo ci)
+        {
+            ci.Balance -= item.ShoppingPrice;
+            _db.SaveChanges();
+        }
+    }
+}
---
+++ WebAPIBank/Models/Init/MyInit.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WebAPIBank.Models.Context;
+using WebAPIBank.Models.Entities;
+
+namespace WebAPIBank.Models.Init
+{
+    public class MyInit:CreateDatabaseIfNotExists<MyContext>
+    {
+        protected override void Seed(MyContext context)
+        {
+            CardInfo ci = new CardInfo();
+            ci.CardUserName = "Ceren Öztunç";
+            ci.CardNumber = "1111 1111 1111 1111";
+            ci.CardExpiryYear = 2024;
+            ci.CardExpiryMonth = 12;
+            ci.SecurityNumber = "222";
+            ci.Limit = 50000;
+            ci.Balance = 50000;
+            context.Cards.Add(ci);
+            context.SaveChanges();
+        }
+    }
+}
